2552af8ced9b8a05ebb68bec6d04d14b02fc92eb
Beso-07 <mahmoudbassem34@gmail.com>
2026-04-08 13:25:59 +0200

    provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main(){
-  runApp(ProviderScope(child: TestState()));
+void main() {
+  runApp(
+    ProviderScope(
+      // provider scope is used to manage the state of the providers in the app
+      // provider scope must be the root of the app, it will be used to manage the state of the providers in the apps
+      // overrides is used to override the providers in the app, we can use it to mock the providers in the tests 
+      overrides: [
+        nameProvider.overrideWithValue('Beso'),
+        ageProvider.overrideWithValue(30),
+
+      ],
+      
+      child: TestState()));
 }
+
 class TestState extends StatelessWidget {
   const TestState({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
 
@@ -25,11 +34,10 @@ class HomeScreen extends ConsumerWidget {
     final age = ref.watch(ageProvider);
     return Scaffold(
       appBar: AppBar(title: Text('Provider State Management')),
-      body: Center(
-        child: Text('Name: $name, Age: $age'),
-      ),
+      body: Center(child: Text('Name: $name, Age: $age')),
     );
   }
 }
+
 final nameProvider = Provider<String>((ref) => 'Mahmoud Bassem');
 final ageProvider = Provider<int>((ref) => 22);
