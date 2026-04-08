d0abcdb69366a48d05dba90e4c0443f4e56c328f
Beso-07 <mahmoudbassem34@gmail.com>
2026-04-08 12:47:26 +0200

    Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main(){
-  runApp(TestState());
+  runApp(ProviderScope(child: TestState()));
 }
 class TestState extends StatelessWidget {
   const TestState({super.key});
@@ -10,7 +11,25 @@ class TestState extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider);
+    final age = ref.watch(ageProvider);
+    return Scaffold(
+      appBar: AppBar(title: Text('Provider State Management')),
+      body: Center(
+        child: Text('Name: $name, Age: $age'),
+      ),
+    );
+  }
+}
+final nameProvider = Provider<String>((ref) => 'Mahmoud Bassem');
+final ageProvider = Provider<int>((ref) => 22);
